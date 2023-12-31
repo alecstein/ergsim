19e7f1717c2340d80a52d0c40454bc9d49c3a16f
Alec Stein <alecrstein@gmail.com>
2023-12-31 13:40:19 -0500

    added python plotter

---
+++ stitch.py
@@ -0,0 +1,45 @@
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+
+# Parse args from command line
+import argparse
+parser = argparse.ArgumentParser(description='Plot position distribution from output file')
+parser.add_argument('n', type=int, nargs=1, help='Number of particles')
+parser.add_argument('t', type=int, nargs=1, help='Number of timesteps')
+args = parser.parse_args()
+n = args.n[0]
+t = args.t[0]
+
+
+filename = f"N_{n}_Time_{t}_out.txt"
+f = open(filename, 'r')
+raw_data = []
+for line in f:
+    raw_data.append(float(line.strip()))
+
+# break the raw data into subslices
+data = [raw_data[i:i + n] for i in range(0, len(raw_data), n)]
+
+fig, ax = plt.subplots(figsize=(8, 5))
+
+bin_ct = 100
+bin_arr = [0.5 * x for x in range(-bin_ct, bin_ct)]
+
+
+def animate(i):
+    ax.clear()
+    ax.hist(data[i], density=True, histtype='step', bins=bin_arr)
+    ax.set_title(f"Position distribution, ${n}$ particles\n $v_0 = const$")
+    ax.set_xlabel("$x$ (m)")
+    ax.set_ylabel("$p(x)$")
+    ax.set_ylim(0, 1)
+    # ax.set_xlim(0, 1.4)
+
+
+ani = animation.FuncAnimation(fig,
+                              animate,
+                              frames=len(data),
+                              repeat=True,
+                              interval=50)
+ani.save(f'animation_{n}_{t}_positions.mp4')
+plt.show()
