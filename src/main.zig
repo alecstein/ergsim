0a33a3382110e89064fa1786cf58834425504543
Alec Stein <alecrstein@gmail.com>
2024-01-01 17:11:58 -0500

    works

--- src/main.zig
+++ src/main.zig
@@ -3,8 +3,8 @@ const math = std.math;
 const Allocator = std.mem.Allocator;
 const alloc = std.heap.page_allocator;
 
-const tracer = @import("tracer");
-pub const tracer_impl = tracer.spall;
+// const tracer = @import("tracer");
+// pub const tracer_impl = tracer.spall;
 
 const g = 9.81;
 const m_particle = 0.001;
@@ -15,13 +15,12 @@ const gravity = false;
 var piston_x: f64 = 1;
 var piston_v: f64 = 0;
 
+const Col = struct {
+    time: f64,
+    type: bool,
+};
+
 pub fn main() !void {
-    try tracer.init();
-    defer tracer.deinit();
-
-    try tracer.init_thread(std.fs.cwd());
-    defer tracer.deinit_thread();
-
     var timer = try std.time.Timer.start();
     const t0 = timer.read();
 
@@ -33,16 +32,15 @@ pub fn main() !void {
 
     var xs = try alloc.alloc(f64, n);
     var vs = try alloc.alloc(f64, n);
-    var t_grounds = try alloc.alloc(f64, n);
-    var t_pistons = try alloc.alloc(f64, n);
+    var cols = try alloc.alloc(Col, n);
+
     defer {
         alloc.free(xs);
         alloc.free(vs);
-        alloc.free(t_grounds);
-        alloc.free(t_pistons);
+        alloc.free(cols);
     }
 
-    initializeArrays(&xs, &vs, &t_grounds, &t_pistons, n);
+    initializeArrays(n, &xs, &vs, &cols);
 
     const est_total_items = @as(usize, @intFromFloat(@divFloor(max_time, 10)));
     var pct_done: u8 = 0;
@@ -56,17 +54,18 @@ pub fn main() !void {
     while (t < max_time) {
         ct += 1;
 
-        const int = getNextInteraction(t_grounds, t_pistons, n);
+        const int = getNextInteraction(cols);
         const dt = int.dt;
         const j = int.j;
         const ground = int.is_ground_col;
 
-        stepForward(&xs, &vs, &t_grounds, n, dt);
+        // advance the particles forward
+        advanceSystem(dt, xs, vs);
 
         if (ground) {
-            handleGroundCollision(&xs, &vs, &t_grounds, &t_pistons, n, j, dt);
+            handleGroundCollision(j, dt, xs, vs, cols);
         } else {
-            handlePistonCollision(&xs, &vs, &t_grounds, &t_pistons, n, j);
+            handlePistonCollision(j, dt, xs, vs, cols);
         }
 
         updateProgress(root_node, &pct_done, t, max_time, est_total_items);
@@ -95,7 +94,7 @@ fn initVel() f64 {
     return v;
 }
 
-fn initializeArrays(xs: *[]f64, vs: *[]f64, t_grounds: *[]f64, t_pistons: *[]f64, n: usize) void {
+fn initializeArrays(n: usize, xs: *[]f64, vs: *[]f64, cols: *[]Col) void {
     // without a seed, this will always produce the same sequence
     // this is good for debugging, but a more robust
     // solution might be better long-term.
@@ -105,15 +104,22 @@ fn initializeArrays(xs: *[]f64, vs: *[]f64, t_grounds: *[]f64, t_pistons: *[]f64
     for (0..n) |i| {
         xs.*[i] = piston_x * rand.float(f32);
         vs.*[i] = initVel();
-        t_grounds.*[i] = getTimeToGround(xs.*[i], vs.*[i]);
-        t_pistons.*[i] = getTimeToPiston(xs.*[i], vs.*[i]);
+
+        const t_p = getTimeToPiston(xs.*[i], vs.*[i]);
+        const t_g = getTimeToGround(xs.*[i], vs.*[i]);
+        const c = Col{
+            .time = @min(t_p, t_g),
+            .type = t_g < t_p,
+        };
+        cols.*[i] = c;
     }
 }
 
 // get time to hit ground
 fn getTimeToGround(x: f64, v: f64) f64 {
-    const t_ = tracer.trace(@src(), "getTimeToGround", .{});
-    defer t_.end();
+    // const t_ = tracer.trace(@src(), "getTimeToGround", .{});
+    // defer t_.end();
+
     if (!gravity) {
         if (v >= 0) return math.inf(f64);
         return -x / v;
@@ -128,8 +134,8 @@ fn getTimeToGround(x: f64, v: f64) f64 {
 
 // get time to collide with piston
 fn getTimeToPiston(x: f64, v: f64) f64 {
-    const t_ = tracer.trace(@src(), "getTimeToPiston", .{});
-    defer t_.end();
+    // const t_ = tracer.trace(@src(), "getTimeToPiston", .{});
+    // defer t_.end();
 
     if (!gravity) {
         const a = -0.5 * g;
@@ -147,84 +153,93 @@ fn getTimeToPiston(x: f64, v: f64) f64 {
 
 // compute velocities after elastic collision
 fn elasticCol(m1: f64, v1: f64, m2: f64, v2: f64) struct { v1_prime: f64, v2_prime: f64 } {
-    const t = tracer.trace(@src(), "elasticCol", .{});
-    defer t.end();
+    // const t = tracer.trace(@src(), "elasticCol", .{});
+    // defer t.end();
 
     const v1_prime = (m1 - m2) * v1 / (m1 + m2) + 2 * m2 * v2 / (m1 + m2);
     const v2_prime = 2 * m1 * v1 / (m1 + m2) - (m1 - m2) * v2 / (m1 + m2);
     return .{ .v1_prime = v1_prime, .v2_prime = v2_prime };
 }
 
-fn getNextInteraction(t_grounds: []f64, t_pistons: []f64, n: usize) struct { dt: f64, j: usize, is_ground_col: bool } {
-    const t = tracer.trace(@src(), "getNextInteraction", .{});
-    defer t.end();
+fn getNextInteraction(cols: []Col) struct { dt: f64, j: usize, is_ground_col: bool } {
     // this is always O(N) time, so there's no prettier way to do it
     var dt = math.inf(f64);
     var j: usize = undefined;
     var is_ground_col: bool = true;
 
-    for (0..n) |i| {
-        const trial_t = @min(t_grounds[i], t_pistons[i]);
+    for (cols, 0..) |c, i| {
+        const trial_t = c.time;
         if (trial_t < dt) {
             dt = trial_t;
             j = i;
-            is_ground_col = t_grounds[i] < t_pistons[i];
+            is_ground_col = c.type;
         }
     }
 
     return .{ .dt = dt, .j = j, .is_ground_col = is_ground_col };
 }
 
-fn stepForward(xs: *[]f64, vs: *[]f64, t_grounds: *[]f64, n: usize, dt: f64) void {
-    const t = tracer.trace(@src(), "stepForward", .{});
-    defer t.end();
-    for (0..n) |i| {
+fn advanceSystem(dt: f64, xs: []f64, vs: []f64) void {
+    for (xs, vs) |*x, *v| {
         if (!gravity) {
-            xs.*[i] += vs.*[i] * dt;
+            x.* += v.* * dt;
         } else {
-            xs.*[i] += vs.*[i] * dt - g * dt * dt / 2;
-            vs.*[i] -= g * dt;
+            x.* += v.* * dt - g * dt * dt / 2;
+            x.* -= g * dt;
         }
-        t_grounds.*[i] -= dt;
     }
     piston_x += piston_v * dt - g * dt * dt / 2;
     piston_v -= g * dt;
 }
 
-fn handleGroundCollision(xs: *[]f64, vs: *[]f64, t_grounds: *[]f64, t_pistons: *[]f64, n: usize, j: usize, dt: f64) void {
-    const t = tracer.trace(@src(), "handleGroundCollision", .{});
-    defer t.end();
-    vs.*[j] = -vs.*[j];
+fn handleGroundCollision(j: usize, dt: f64, xs: []f64, vs: []f64, cols: []Col) void {
+    // reverse the velocity of the colliding particle
+    vs[j] = -vs[j];
+
+    // every collision gets dt closer
+    for (cols) |*c| {
+        c.*.time -= dt;
+    }
+
+    // update the next interaction for the particle that just collided with the ground
     if (!gravity) {
-        t_grounds.*[j] = math.inf(f64);
+        // without gravity, the next collision is _always_ with the piston
+        cols[j].time = math.inf(f64);
+        cols[j].type = false;
     } else {
-        t_grounds.*[j] = getTimeToGround(xs.*[j], vs.*[j]);
+        // under gravity it could be either
+        const t_g = getTimeToGround(xs.*[j], vs.*[j]);
+        const t_p = getTimeToPiston(xs.*[j], vs.*[j]);
+        cols[j].time.* = @min(t_g, t_p);
+        cols[j].type.* = t_g < t_p;
     }
-
-    for (0..n) |i| {
-        t_pistons.*[i] -= dt;
-    }
-
-    t_pistons.*[j] = getTimeToPiston(xs.*[j], vs.*[j]);
 }
 
-fn handlePistonCollision(xs: *[]f64, vs: *[]f64, t_grounds: *[]f64, t_pistons: *[]f64, n: usize, j: usize) void {
-    const t = tracer.trace(@src(), "handlePistonCollision", .{});
-    defer t.end();
-
-    const new_vs = elasticCol(m_particle, vs.*[j], m_piston, piston_v);
-    vs.*[j] = new_vs.v1_prime;
+fn handlePistonCollision(j: usize, dt: f64, xs: []f64, vs: []f64, cols: []Col) void {
+    const new_vs = elasticCol(m_particle, vs[j], m_piston, piston_v);
+    vs[j] = new_vs.v1_prime;
     piston_v = new_vs.v2_prime;
-    t_grounds.*[j] = getTimeToGround(xs.*[j], vs.*[j]);
 
-    for (0..n) |i| {
-        t_pistons.*[i] = getTimeToPiston(xs.*[i], vs.*[i]);
+    // if a particle is going to hit the ground, there's no need to recompute its
+    // collision time with the piston. A piston colliding with any particle is not
+    // going to make the piston move downward faster.
+
+    for (cols, 0..) |*c, i| {
+        // a ground collision is simple:
+        if (c.*.type == true) {
+            c.*.time -= dt;
+        } else {
+            const t_g = getTimeToGround(xs[i], vs[i]);
+            const t_p = getTimeToPiston(xs[i], vs[i]);
+            c.*.time = @min(t_g, t_p);
+            c.*.type = t_g < t_p;
+        }
     }
 }
 
 fn updateProgress(root_node: *std.Progress.Node, pct_done: *u8, t: f64, max_time: f64, est_total_items: usize) void {
-    const t_ = tracer.trace(@src(), "updatProgress", .{});
-    defer t_.end();
+    // const t_ = tracer.trace(@src(), "updatProgress", .{});
+    // defer t_.end();
     const frac_time = (t * @as(f64, @floatFromInt(est_total_items))) / max_time;
     const int_frac_time = @as(u8, @intFromFloat(frac_time));
 
